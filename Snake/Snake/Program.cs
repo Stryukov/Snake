8c9fb92a2b82c8c01420e4cecef672e1b8587737
Stryukov <stryukov.semen@gmail.com>
2021-06-05 01:05:24 +1200

    Lines

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,6 +13,12 @@ namespace Snake
             Point p2 = new Point(4, 5,'#');
             p2.Draw();
 
+            HorizontalLine hLine = new HorizontalLine(4, 7, 7, '-');
+            hLine.Draw();
+
+            VerticalLIne vLine = new VerticalLIne(8, 3, 8, '|');
+            vLine.Draw();
+
             Console.ReadLine();
         }
 
---
+++ Snake/Snake/VerticalLIne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLIne
+    {
+        List<Point> pList;
+
+        public VerticalLIne (int x, int yBottom, int yTop, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yBottom; y <= yTop; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
