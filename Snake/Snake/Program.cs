f9fc290670f379f85230439e9f374c421efbe7b6
Stryukov <stryukov.semen@gmail.com>
2021-06-08 23:50:58 +1200

    snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -7,17 +7,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            Point p = new Point(4,5,'*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-            Point p2 = new Point(4, 5,'#');
-            p2.Draw();
 
-            HorizontalLine hLine = new HorizontalLine(4, 7, 7, '-');
+            /*HorizontalLine hLine = new HorizontalLine(4, 7, 7, '-');
             hLine.Draw();
 
             VerticalLIne vLine = new VerticalLIne(8, 3, 8, '|');
-            vLine.Draw();
+            vLine.Draw();*/
 
             Console.ReadLine();
         }
